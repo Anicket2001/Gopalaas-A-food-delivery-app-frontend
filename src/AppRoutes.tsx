0fb973e9ca2c8ef50a547d740e5bb9cd2370d136
Anicket2001 <anicketshardha@gmail.com>
2024-08-20 19:02:14 +0530

    deploying search and sort feature fourth commit

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,6 +5,7 @@ import AuthCallbackPage from './pages/AuthCallbackPage'
 import UserProfilePage from './pages/UserProfilePage'
 import ProtectedRoute from './auth/ProtectedRoute'
 import ManageRestaurantPage from './pages/ManageRestaurantPage'
+import SearchPage from './pages/SearchPage'
 
 
 function AppRoutes() {
@@ -13,6 +14,31 @@ function AppRoutes() {
       <Routes>
         <Route path='/' element={<Layout showHero><HomePage /></Layout>} />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+        <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+      {/* <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        /> */}
         <Route element={<ProtectedRoute />}>
         <Route 
         path='/user-profile'
